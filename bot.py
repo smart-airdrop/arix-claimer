0d18272bb841148f4b2c37d453cf7aabb1620eb6
Smart Airdrop <smileuptn@gmail.com>
2024-06-28 14:03:48 +0700

    bot-v2

--- bot.py
+++ bot.py
@@ -20,25 +20,9 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 data_file = os.path.join(script_dir, "data.txt")
 
 
-# Clear the terminal
-def clear_terminal():
-    # For Windows
-    if os.name == "nt":
-        _ = os.system("cls")
-    # For macOS and Linux
-    else:
-        _ = os.system("clear")
-
-
 class ArixDEX:
-    def arix_claimer(self, telegram_id):
-        url = f"https://miner-webapp-pi.vercel.app/api/claim?id={telegram_id}"
-
-        headers = {
-            "authority": "miner-webapp-pi.vercel.app",
-            "medthod": "POST",
-            "path": f"/api/claim?id={telegram_id}",
-            "scheme": "https",
+    def __init__(self):
+        self.headers = {
             "Accept": "application/json, text/plain, */*",
             "Accept-Encoding": "gzip, deflate, br, zstd",
             "Accept-Language": "en-US,en;q=0.9",
@@ -58,29 +42,80 @@ class ArixDEX:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
         }
 
-        response = requests.post(url, headers=headers)
-        if response.status_code == 200:
-            print(f"{green}Claim: Done")
+        self.line = white + "~" * 50
+
+        self.banner = f"""
+        {blue}Smart Airdrop {white}ArixDEX Auto Claimer
+        t.me/smartairdrop2120
+        
+        """
+
+    # Clear the terminal
+    def clear_terminal(self):
+        # For Windows
+        if os.name == "nt":
+            _ = os.system("cls")
+        # For macOS and Linux
         else:
-            print(f"{red}Error! Try again")
+            _ = os.system("clear")
+
+    def user_info(self, telegram_id):
+        url = f"https://miner-webapp-fz9k.vercel.app/api/user?id={telegram_id}"
+
+        headers = self.headers
+
+        response = requests.get(url, headers=headers)
+
+        return response
+
+    def arix_claimer(self, telegram_id):
+        url = f"https://miner-webapp-pi.vercel.app/api/claim?id={telegram_id}"
+
+        headers = self.headers
+
+        response = requests.post(url, headers=headers)
+
+        return response
+
+    def log(self, msg):
+        now = datetime.now().isoformat(" ").split(".")[0]
+        print(f"{black}[{now}]{reset} {msg}{reset}")
 
     def main(self):
-        clear_terminal()
-        banner = f"""
-    {blue}Smart Airdrop {white}ArixDEX Auto Claimer
-    t.me/smartairdrop2120
-    
-        """
-        print(banner)
+        self.clear_terminal()
+        print(self.banner)
         data = open(data_file, "r").read().splitlines()
+        num_acc = len(data)
+        self.log(self.line)
+        self.log(f"{green}Numer of account: {white}{num_acc}")
         while True:
             for no, telegram_id in enumerate(data):
-                now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                print(f"{black}[{now}] {white}--- Account Number: {no+1} ---")
-                self.arix_claimer(telegram_id=telegram_id)
-                print()
+                self.log(self.line)
+                self.log(f"{green}Account number: {white}{no+1}/{num_acc}")
+                try:
+                    user_info = self.user_info(telegram_id=telegram_id).json()
+                    first_name = user_info["first_name"]
+                    last_name = user_info["last_name"]
+                    user_name = user_info["username"]
+                    tele_id = user_info["id"]
+                    balance = user_info["balance"]
+                    self.log(
+                        f"{green}Account Info: {white}{first_name} {last_name} ({user_name} - {tele_id})"
+                    )
+                    self.log(f"{green}Balance: {white}{balance}")
+                except Exception as e:
+                    self.log(f"{red}Get user info error!!!", e)
+
+                try:
+                    claim = self.arix_claimer(telegram_id=telegram_id).json()
+                    claim_balance = claim["balance"]
+                    self.log(f"{green}Balance after Claim: {white}{claim_balance}")
+                except Exception as e:
+                    self.log(f"{red}Claim error!!!", e)
+
+            print()
             wait_time = 30 * 60
-            print(f"{yellow}Wait for {int(wait_time/60)} minutes!")
+            self.log(f"{yellow}Wait for {int(wait_time/60)} minutes!")
             time.sleep(wait_time)
 
 
